cefbc4247498eb111a1bea0b0447487e68fe111a
Georg Jung <git@gjung.com>
2021-10-03 01:09:20 +0200

    Fix wasm client automapper profile

--- src/AV.AvA.BlazorWasmClient/AutoMapperProfile.cs
+++ src/AV.AvA.BlazorWasmClient/AutoMapperProfile.cs
@@ -1,26 +1,33 @@
 ﻿using System.Text.Json;
 using AutoMapper;
 using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
 using Model = AV.AvA.Model;
 
 namespace AV.AvA.BlazorWasmClient
 {
     public class AutoMapperProfile : Profile
     {
-        private readonly JsonSerializerOptions _jsonOpts;
+        private static readonly JsonSerializerOptions _jsonOpt = GetNodaTimeSTJSettings();
 
-        public AutoMapperProfile(JsonSerializerOptions jsonOpts)
+        public AutoMapperProfile()
         {
             CreateMap<PersonVersionReply, Model.PersonVersion>()
                 .ForMember(
                     dest => dest.Person,
                     opt => opt.MapFrom((src, dest) =>
-                        JsonSerializer.Deserialize<Model.Person>(src.Person, _jsonOpts)))
+                        JsonSerializer.Deserialize<Model.Person>(src.Person, _jsonOpt)))
                 .ForMember(
                     dest => dest.CommittedAt,
                     opt => opt.MapFrom((src, dest) =>
                         Instant.FromDateTimeOffset(src.CommittedAt.ToDateTimeOffset())));
-            _jsonOpts = jsonOpts;
+        }
+
+        private static JsonSerializerOptions GetNodaTimeSTJSettings()
+        {
+            var x = new JsonSerializerOptions();
+            x.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+            return x;
         }
     }
 }
