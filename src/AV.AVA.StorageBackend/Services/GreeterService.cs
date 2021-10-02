c6ef66973f89cca1dec32157db1ec40e276f203f
Georg Jung <git@gjung.com>
2021-10-02 10:39:49 +0200

    Add StorageBackend project template/skeleton

---
+++ src/AV.AVA.StorageBackend/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using AV.AVA.StorageBackend;
+using Grpc.Core;
+
+namespace AV.AVA.StorageBackend.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
