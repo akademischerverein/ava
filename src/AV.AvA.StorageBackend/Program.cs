216bc5346c67861ff0b8c1512210eeb1dd66775f
Georg Jung <git@gjung.com>
2021-10-02 18:12:56 +0200

    Enable gRPC-Web, CORS allow all

--- src/AV.AvA.StorageBackend/Program.cs
+++ src/AV.AvA.StorageBackend/Program.cs
@@ -4,6 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
 var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddNpgsql<AvADbContext>(
@@ -30,9 +38,12 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseGrpcWeb();
+app.UseCors();
+
 // Configure the HTTP request pipeline.
-app.MapGrpcService<PersonVersion>();
-app.MapGrpcService<LoginService>();
+app.MapGrpcService<PersonVersion>().EnableGrpcWeb().RequireCors("AllowAll");
+app.MapGrpcService<LoginService>().EnableGrpcWeb().RequireCors("AllowAll");
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
