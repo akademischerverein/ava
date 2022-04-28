e865a3be2742982f65ee0ecdc0a55185d78f7498
Georg Jung <git@gjung.com>
2022-04-28 12:28:43 +0200

    Apply Material Design Theme

---
+++ src/AV.AvA.BlazorWasmClient/Shared/AvaTheme.cs
@@ -0,0 +1,17 @@
+﻿using MudBlazor;
+
+namespace AV.AvA.BlazorWasmClient.Shared
+{
+    internal static class AvaTheme
+    {
+        public static readonly MudTheme Default = new()
+        {
+            Palette = new Palette()
+            {
+                Primary = Colors.Red.Darken2,
+                Secondary = Colors.Grey.Darken1,
+                AppbarBackground = Colors.Red.Darken2,
+            },
+        };
+    }
+}
