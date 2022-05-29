855d95764a7a79cc371766223c9abf69adbc2c1f
Georg Jung <git@gjung.com>
2022-05-29 19:07:47 +0200

    Use nicer names for status event types

---
+++ src/AV.AvA.BlazorWasmClient/Extensions/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace AV.AvA.BlazorWasmClient.Extensions
+{
+    public static class EnumExtensions
+    {
+        // taken from https://stackoverflow.com/a/26455406/1200847
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            ?.GetName() ?? enumValue.ToString();
+        }
+    }
+}
--- src/AV.AvA.Model/StatusEreignisTyp.cs
+++ src/AV.AvA.Model/StatusEreignisTyp.cs
@@ -1,29 +1,78 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AV.AvA.Model;
 
 public enum StatusEreignisTyp
 {
+    [Display(Name = "Studentischer Gast: eingezogen")]
     StudentischerGastEingezogen = 1,
+
+    [Display(Name = "Studentischer Gast: ausgezogen")]
     StudentischerGastAusgezogen,
+
+    [Display(Name = "Jungmitglied: eingesungen")]
     JungmitgliedEingesungen,
+
+    [Display(Name = "Verkehrsgast: aufgenommen")]
     VerkehrsgastAufgenommen,
+
+    [Display(Name = "Vollmitglied: aufgenommen")]
     VollmitgliedAufgenommen,
+
+    [Display(Name = "Inaktivierung")]
     Inaktivierung,
+
+    [Display(Name = "Beurlaubt")]
     Beurlaubt,
+
+    [Display(Name = "Übertritt")]
     Uebertritt,
+
+    [Display(Name = "Ex-Aktiv")]
     ExAktivierung,
+
+    [Display(Name = "Stud. Mitglied HG: Eintritt")]
     EintrittStudentischesMitgliedHG,
+
+    [Display(Name = "Stud. Mitglied AV e.V.: Eintritt")]
     EintrittStudentischesMitgliedAVeV,
+
+    [Display(Name = "Ord. Mitglied HG: Eintritt")]
     EintrittOrdentlichesMitgliedHG,
+
+    [Display(Name = "Ord. Mitglied AV e.V.: Eintritt")]
     EintrittOrdentlichesMitgliedAVeV,
+
+    [Display(Name = "Förd. Mitglied HG: Eintritt")]
     EintrittFoerderndesMitgliedHG,
+
+    [Display(Name = "Förd. Mitglied AV e.V.: Eintritt")]
     EintrittFoerderndesMitgliedAVeV,
+
+    [Display(Name = "Stimmrecht HG erhalten")]
     StimmrechtHGErhalten,
+
+    [Display(Name = "GAV: Eintritt")]
     EintrittGAV,
+
+    [Display(Name = "Ehrenmitgliedschaft erhalten")]
     EhrenmitgliedschaftErhalten,
+
+    [Display(Name = "Verwittwert")]
     Verwittwert,
+
+    [Display(Name = "Verstorben")]
     Verstorben,
+
+    [Display(Name = "Aktivitas: ausgeschieden")]
     AusscheidenAktivitas,
+
+    [Display(Name = "AV e.V.: ausgeschieden")]
     AusscheidenAVeV,
+
+    [Display(Name = "HG: ausgeschieden")]
     AusscheidenHG,
+
+    [Display(Name = "GAV: ausgeschieden")]
     AusscheidenGAV,
 }
